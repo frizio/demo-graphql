d28779c991d697d2ab9f759799ebeb76a1da5774
Maurizio La Rocca <mrz.larocca@gmail.com>
2019-09-03 10:12:37 +0200

    Add home route /

--- src/index.js
+++ src/index.js
@@ -4,6 +4,15 @@ const app = express();
 
 const port = 3000;
 
+app.get(
+    '/', 
+    (req, res) => {
+        res.json(
+            {message: 'Hello World!'}
+        )
+    }
+);
+
 app.listen(
     port,
     () => {
