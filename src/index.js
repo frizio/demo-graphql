4ad7192fd3805ef9168fec4cbe985a5b0c3c26cc
Maurizio La Rocca <mrz.larocca@gmail.com>
2019-09-03 10:17:34 +0200

    Add basic graphql code

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 import express from "express";
+import graphqlHTTP from "express-graphql";
 
 const app = express();
+
+// Middleware
+const schema = {};
+ 
+app.use(
+    '/graphql',
+    graphqlHTTP(
+        {
+            graphiql: true,
+            schema: schema
+        }
+    )
+);
 
 const port = 3000;
 
